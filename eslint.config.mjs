58a8f71061fe79f6ded0da5d64e11f62e8dcb408
drawerwrangler <char.js.lin@gmail.com>
2025-01-10 18:47:54 +0900

    Temporarily disable eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,14 +9,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      "@typescript-eslint/no-unused-vars": isProduction ? "error" : "off",
-      "@typescript-eslint/no-explicit-any": isProduction ? "error" : "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   }),
 ];
