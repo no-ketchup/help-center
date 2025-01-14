1e1a2f1d648da3e524ed53b4a2455bf4e7a0d5f5
drawerwrangler <char.js.lin@gmail.com>
2025-01-14 16:56:50 +0900

    Update routes params.

--- src/components/navigation/nav-utils.ts
+++ src/components/navigation/nav-utils.ts
@@ -3,7 +3,7 @@ import { Category } from "@/types/Category";
 export const mapCategoriesToNavItems = (categories: Category[]) => {
     return categories.map((category) => ({
         title: category.name,
-        url: `/user-guide/category/${category.slug}`,
+        url: `/category/${category.slug}`,
         description: category.description || "No description available.",
     }));
 };
